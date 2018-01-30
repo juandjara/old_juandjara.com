a057d045076cd9507e8fa96baf96631dbf74e9cb
Juan D. Jara <juanorigami@gmail.com>
2018-01-31 00:47:58 +0100

    refactor main react component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import projects from './projects.json';
 
-class App extends Component {
-  state = {projects}
-  render() {
-    return (
-      <ul>
-        {this.state.projects.map(project => (
-          <li key={project.link}>
-            <a href={project.link}>
-              <img src={project.image} alt=""/>
-              <div>
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-              </div>
-            </a>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+function App() {
+  return (
+    <ul>
+      {projects.map(project => (
+        <li key={project.link}>
+          <a href={project.link}>
+            <img src={project.image} alt=""/>
+            <div>
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </div>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
